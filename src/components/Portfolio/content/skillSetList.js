c34859f926615602f6fe02c1fd0fb5b037dcfd56
Srilekha Palleboyina <srilekha091@gmail.com>
2025-04-14 13:05:36 -0700

    Adding skill-set section

---
+++ src/components/Portfolio/content/skillSetList.js
@@ -0,0 +1,52 @@
+import { FaReact, FaAws, FaHtml5, FaCss3 } from "react-icons/fa";
+import {
+	SiTypescript,
+	SiJavascript,
+	SiChakraui,
+	SiRedux,
+	SiReactrouter,
+	SiPostgresql,
+} from "react-icons/si";
+
+const SkillSet = [
+	{
+		icon: FaReact,
+		skillName: "React",
+	},
+	{
+		icon: SiTypescript,
+		skillName: "TypeScript",
+	},
+	{
+		icon: SiJavascript,
+		skillName: "JavaScript",
+	},
+	{
+		icon: SiRedux,
+		skillName: "Redux",
+	},
+	{
+		icon: SiReactrouter,
+		skillName: "React-Router",
+	},
+	{
+		icon: SiPostgresql,
+		skillName: "Postgresql",
+	},
+	{
+		icon: FaHtml5,
+		skillName: "HTML5",
+	},
+	{
+		icon: FaCss3,
+		skillName: "CSS3",
+	},
+	{
+		icon: SiChakraui,
+		skillName: "Chakra UI",
+	},
+	{
+		icon: FaAws,
+		skillName: "AWS",
+	},
+];
